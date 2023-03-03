3e77a39791fa5290502c836227408b1ca5aea8ea
user <clintononitsha20@gmail.com>
2023-03-03 19:06:24 +0100

    made new corrections

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<nellobyte>'));
-app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/<nellobyte>/index.html'));});
+app.use(express.static(__dirname + '/dist/nellobyte'));
+app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/nellobyte/index.html'));});
 
 app.listen(process.env.PORT || 8080, ()=>{
     console.log("Hello World");
