c0e764e6695e497e17547c23f34fd5a4a1278add
user <clintononitsha20@gmail.com>
2023-03-03 18:44:27 +0100

    just pushed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname+'/dist/<nellobyte>'));
-app.get('/*', function(req,res) {res.send(path.join(__dirname+'/dist/<nellobyte>/src/index.html'));});
+app.get('/*', function(req,res) {res.send(path.join(__dirname+'/dist/<nellobyte>/index.html'));});
 
 app.listen(process.env.PORT || 8080, ()=>{
     console.log("Hello World");
